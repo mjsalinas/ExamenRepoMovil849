9ea0f463b502698567bcfc6e001151ad6fffe88d
Luisponce17 <lr8585ponce@gmail.com>
2026-06-26 20:17:12 -0600

    Inciso C.1 - Persistencia - se despacha fetchExpenses al iniciar la app mediante AppLoader para cargar gastos desde AsyncStorage

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import { store } from "./src/store";
+import { fetchExpenses } from "./src/store/expensesSlice";
+import type { AppDispatch } from "./src/store";
 
 import { LanguageProvider } from "./src/context/LanguageContext";
 import { ThemeProvider, useTheme } from "./src/context/ThemeContext";
@@ -19,13 +21,23 @@ function ThemedApp() {
   );
 }
 
+function AppLoader() {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchExpenses());
+  }, [dispatch]);
+
+  return <ThemedApp />;
+}
+
 export default function App() {
   return (
     <SafeAreaProvider>
       <ThemeProvider>
         <LanguageProvider>
           <Provider store={store}>
-            <ThemedApp />
+            <AppLoader />
           </Provider>
         </LanguageProvider>
       </ThemeProvider>
